fc54b694abf0fb71e0fec125291bcd5e0fa88ef1
Douglas Lopes <douglaslopsou@gmail.com>
2025-05-14 13:43:58 -0300

    fix: inject movie

--- src/producer/producer.module.ts
+++ src/producer/producer.module.ts
@@ -3,9 +3,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Producer } from './producer.entity';
 import { ProducerService } from './producer.service';
 import { ProducerController } from './producer.controller';
+import { Movie } from 'src/movie/movie.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Producer])],
+  imports: [TypeOrmModule.forFeature([Producer, Movie])],
   providers: [ProducerService],
   controllers: [ProducerController],
 })
